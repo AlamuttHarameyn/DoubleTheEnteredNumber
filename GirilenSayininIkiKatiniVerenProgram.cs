396279c7479a33d1dc71758f74c1c9f3a66f5797
Gizem <gizemustahuseyin123@gmail.com>
2023-01-09 19:30:56 +0300

    Add files via upload

---
+++ GirilenSayininIkiKatiniVerenProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._9_SozDizilimiVeTemelProgramlama
+{
+    class GirilenSayininIkiKatiniVerenProgram
+    {
+        static void Main(string[] args)
+        {
+            int x = 2; //X 2 olarak tanımlanıyor.Girilen sayı 2
+            int y = 2 * x; //Y 2*x olarak tanımlanıyor.
+            Console.WriteLine(y); //Kodu sonlandırıyor. artık program çalıştırılabilir.
+            Console.ReadKey(); //Program kapanmadan önce bir tuşa basmamı istesin.
+        }
+    }
+}
